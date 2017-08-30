4692fc009d48e6156d019ab506c9e5a375b0f090
2068 <2068314880@qq.com>
2017-08-30 17:35:34 +0800

    Update shijiancha.c

--- shijiancha.c
+++ shijiancha.c
@@ -8,6 +8,6 @@ int main()
 	int t1 = hour1 * 60 + minute1;
 	int t2 = hour2 * 60 + minute2;
 	int t = t1 - t2;
-	printf("时间差是%d小时%d分", t / 60, t % 60);
+	printf("%dh%dm", t / 60, t % 60);
 	return 0;
 }
